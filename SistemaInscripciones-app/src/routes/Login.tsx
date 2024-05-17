4a81d74ff83955b1c40b74144417a2bd185dd1e6
Maaldobo <maaldobo55@gmail.com>
2024-05-16 20:15:59 -0600

    Delete SistemaInscripciones-app directory
    cambiar archivos para cambiar de Bootstrap a MUI

--- SistemaInscripciones-app/src/routes/Login.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from 'react-router-dom'
-import '../App.css'
-function Login(){
-<>
-    <button><Link to='/Alumno'>Alumno</Link></button>
-    <button><Link to='/Admin'>Admin</Link></button>
-</>
-}
-export default Login
